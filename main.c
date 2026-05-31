ac0193595ea395592756e0460722078039420a39
danmaryn <marynenko.danylo@lll.kpi.ua>
2026-05-31 14:08:15 +0000

    feat: implement daemonization logic

--- main.c
+++ main.c
@@ -1,12 +1,31 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
 #include <time.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+void make_daemon() {
+    pid_t pid = fork();
+    
+    if (pid < 0) exit(EXIT_FAILURE);
+    if (pid > 0) exit(EXIT_SUCCESS);
+
+    if (setsid() < 0) exit(EXIT_FAILURE);
+
+    umask(0);
+    chdir("/");
+
+    close(STDIN_FILENO);
+    close(STDOUT_FILENO);
+    close(STDERR_FILENO);
+}
 
 int main() {
-    FILE *log_file = fopen("daemon.log", "a");
-    if (!log_file) {
-        return 1;
-    }
+    make_daemon();
+
+    FILE *log_file = fopen("/tmp/daemon.log", "a");
+    if (!log_file) return 1;
 
     while (1) {
         time_t now = time(NULL);
