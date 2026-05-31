52335acd5bef64cca6da667bfa38c0947fb291f3
danmaryn <marynenko.danylo@lll.kpi.ua>
2026-05-31 13:57:48 +0000

    feat: log output to file

--- main.c
+++ main.c
@@ -1,10 +1,20 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <time.h>
 
 int main() {
+    FILE *log_file = fopen("daemon.log", "a");
+    if (!log_file) {
+        return 1;
+    }
+
     while (1) {
-        printf("Process is running...\n");
+        time_t now = time(NULL);
+        fprintf(log_file, "Process is running timestamp: %ld\n", now);
+        fflush(log_file);
         sleep(1);
     }
+
+    fclose(log_file);
     return 0;
 }
